d6fe26eb93e30cd19418b4aea6f1e9de385b6492
sarvjeet <sarvjeet908@gmail.com>
2024-11-07 13:05:34 +0530

    bms project models

--- bms/src/main/java/com/example/bms/BmsApplication.java
+++ bms/src/main/java/com/example/bms/BmsApplication.java
@@ -1,5 +1,6 @@
 package com.example.bms;
 
+import com.example.bms.models.BaseModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class BmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BmsApplication.class, args);
+
 	}
 
 }
---
+++ bms/src/main/java/com/example/bms/models/Region.java
@@ -0,0 +1,4 @@
+package com.example.bms.models;
+
+public class Region extends BaseModel{
+}

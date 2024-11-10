7fb15cdb13e3b1c28a8687f8bcd37e0958157e31
sarvjeet <sarvjeet908@gmail.com>
2024-11-10 12:42:52 +0530

    issue fixed by looking in to table created in mysql db

--- bms/src/main/java/com/example/bms/models/User.java
+++ bms/src/main/java/com/example/bms/models/User.java
@@ -1,14 +1,18 @@
 package com.example.bms.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
-
+@Entity
+@Getter
+@Setter
 public class User extends BaseModel{
     private String name;
     private String phoneNumber;
     private String email;
-    @OneToMany
-    private List<Booking> bookings;
+
 
 }
